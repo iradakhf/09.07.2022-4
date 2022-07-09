d0eacf73b4dd1a40a84946dd28bdd643c13b151b
iradakhf <iradakhf@code.edu.az>
2022-07-09 08:12:58 +0400

    add ConsoleApp12

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace Arrays
+{
+    class Program
+    {
+        public static void Main(string [] args)
+        {
+            int[] numbersArray = { 7, 4, 25, 12 };
+            int sum = SumOfFirstNNaturalnumbers(numbersArray, 2);
+            Console.WriteLine(sum);
+        }
+        public static int SumOfFirstNNaturalnumbers(int[] numbers,int n)
+        {
+            int sum = 0;
+            for (int i = 0; i < n; i++)
+            {
+                sum += numbers[i];
+            }
+            return sum;
+        }
+    }
+}
